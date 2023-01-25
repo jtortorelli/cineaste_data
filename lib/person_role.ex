2b8bd3c469e34e78cf1b00f6ce17cb4e2a29a457
James Tortorelli <jtortorelli03@icloud.com>
2023-01-25 10:47:41 -0600

    fix changeset for person role

--- lib/person_role.ex
+++ lib/person_role.ex
@@ -10,7 +10,7 @@ defmodule CineasteData.PersonRole do
     field :name, :string
     field :order, :string
     field :uncredited, :boolean, default: false
-    field :qualifiers, {:array, :string}
+    field :qualifiers, {:array, :string}, default: []
 
     belongs_to :person, Person
     belongs_to :film, Film
@@ -22,7 +22,20 @@ defmodule CineasteData.PersonRole do
   @doc false
   def changeset(person_role, attrs) do
     person_role
-    |> cast(attrs, [:name, :order, :uncredited, :avatar_url, :actor_alias])
-    |> validate_required([:name, :order, :uncredited])
+    |> cast(attrs, [
+      :name,
+      :order,
+      :uncredited,
+      :avatar_url,
+      :actor_alias,
+      :film_id,
+      :person_id,
+      :character_id,
+      :qualifiers
+    ])
+    |> validate_required([:name, :order, :uncredited, :film_id, :person_id])
+    |> assoc_constraint(:person)
+    |> assoc_constraint(:film)
+    |> assoc_constraint(:character)
   end
 end
