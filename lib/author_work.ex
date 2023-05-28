84e42582c8ac8c91fabc3b6f5eb304e76e9904fe
James Tortorelli <jtortorelli03@icloud.com>
2023-05-28 00:14:06 -0500

    missing author work changeset

--- lib/author_work.ex
+++ lib/author_work.ex
@@ -1,9 +1,18 @@
 defmodule CineasteData.AuthorWork do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "author_works" do
     field :person_id, :id
     field :work_id, :id
     timestamps()
+  end
+
+  def changeset(author_work, attrs) do
+    author_work
+    |> cast(attrs, [:person_id, :work_id])
+    |> validate_required([:person_id, :work_id])
+    |> assoc_constraint(:person)
+    |> assoc_constraint(:work)
   end
 end
