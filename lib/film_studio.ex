29e47ef9fe8c9667f7b24ccc3e7108548a7b7025
James Tortorelli <jtortorelli03@icloud.com>
2023-07-15 20:48:54 -0500

    enhance film studio schema

--- lib/film_studio.ex
+++ lib/film_studio.ex
@@ -1,9 +1,20 @@
 defmodule CineasteData.FilmStudio do
   use Ecto.Schema
+  import Ecto.Changeset
+
+  alias CineasteData.{Film, Studio}
 
   schema "film_studios" do
-    field :film_id, :id
-    field :studio_id, :id
+    belongs_to :film, Film
+    belongs_to :studio, Studio
     timestamps()
+  end
+
+  def changeset(film_studio, attrs) do
+    film_studio
+    |> cast(attrs, [:film_id, :studio_id])
+    |> validate_required([:film_id, :studio_id])
+    |> assoc_constraint(:film)
+    |> assoc_constraint(:studio)
   end
 end
